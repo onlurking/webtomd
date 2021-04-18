c76de10b9f5e22be09ff267ab8b5088d289feaf2
Diogo Felix <diogo2fex@gmail.com>
2021-04-18 01:45:01 -0300

    feat: add output option

--- index.js
+++ index.js
@@ -3,49 +3,54 @@
 const readability = require("readability");
 const JSDOM = require("jsdom").JSDOM;
 const requestPromise = require("request-promise-native");
-var TurndownService = require('turndown')
-writeFile = require('fs').writeFile;
+var TurndownService = require("turndown");
+writeFile = require("fs").writeFile;
 
 var turndownService = new TurndownService({
-  bulletListMarker: '-',
+  bulletListMarker: "-",
   codeBlockStyle: "fenced",
   emDelimiter: "*",
-})
+});
 
-const [,, ...args] = process.argv;
+const [, , ...args] = process.argv;
 const url = args[0];
+const output = args[1];
 
 async function getContent() {
-    const body = await requestPromise(url, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0'
-      }
-    });
+  const body = await requestPromise(url, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0",
+    },
+  });
 
-    const doc = new JSDOM(body, {
-        url: url,
-    });
+  const doc = new JSDOM(body, {
+    url: url,
+  });
 
-    const reader = new readability.Readability(doc.window.document);
-    const article = reader.parse();
+  const reader = new readability.Readability(doc.window.document);
+  const article = reader.parse();
 
-    return article
+  return article;
 }
 
 async function main() {
-  const article = await (await getContent())
-  const markdown = turndownService.turndown(article.content.toString() )
-  const contents = `# ${article.title}\n\n${markdown}\n\n[source](${url})\n`
+  const article = await await getContent();
+  const markdown = turndownService.turndown(article.content.toString());
+  const contents = `# ${article.title}\n\n${markdown}\n\n[source](${url})\n`;
 
-  const name = article.title.replace('/', '-')
+  if (!output) {
+    const name = article.title.replace("/", "-");
 
-  writeFile(`${name}.md`, contents, function (err) {
-    if (err) return console.log(err);
+    writeFile(`${name}.md`, contents, function (err) {
+      if (err) return console.log(err);
       console.log(`${article.title} > ${name}.md`);
     });
+  } else {
+    console.log(contents);
+  }
 }
 
-if(args.length > 0) {
-  main()
+if (args.length > 0) {
+  main();
 }
-
